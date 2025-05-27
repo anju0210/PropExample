1480239b2712a6a3fbbbd7ae6bf127350b10e06b
anju0210 <s2309@e-mirim.hs.kr>
2025-05-27 14:51:05 +0900

    Box 클래스 게터세터 구현

--- Box.cs
+++ Box.cs
@@ -24,6 +24,18 @@ namespace PropExample
             }
         }
 
+        public int getWidth() { return this.width; }
+        public int getHeight() { return this.height; }
+        public void setWidth(int w)
+        {
+            this.width = w;
+        }
+
+        public void setHeight(int h)
+        {
+            this.height = h;
+        }
+
         public int Area()
         {
             return this.width * this.height;
